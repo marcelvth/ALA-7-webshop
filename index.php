9be2b137053a41f8a237860c5da40b830cfed240
marcelvth <37689054+marcelvth@users.noreply.github.com>
2019-04-24 10:49:14 +0200

    index menu structuur

--- index.php
+++ index.php
@@ -1,12 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Otto Graaf</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+	session_start();
+	require("includes/connection.php");
+	$id = $_SESSION['id'];
+	if (isset($id)) {
+		$_head = 'header1';
+	} else {
+		$_head = 'header';
+	}
+
+	require($_head.".php");
+
+	if(isset($_GET['page'])){
+		$pages=array("products", "cart","home","menu","uitloggen","inloggen","register", "Account");
+		if(in_array($_GET['page'], $pages)) {
+			$_page=$_GET['page'];
+		}else{
+			$_page="home";
+		}
+	}
+	if ($_page == "foto" || $_page == "video" ) {
+		include_once "mand.php";
+	}
+?>	
+
+<div class="content">
+	<div class="wrapper">
+		<?php require($_page.".php"); ?>
+	</div>
+</div>
+<?php include 'pages/footer.php';?>
