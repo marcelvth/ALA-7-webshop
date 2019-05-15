3f2def9d97b61fddbf35667ec73ff712c34b7d65
marcelvth <37689054+marcelvth@users.noreply.github.com>
2019-05-15 09:13:56 +0200

    update

--- includes/connection.php
+++ includes/connection.php
@@ -1,7 +1,16 @@
 <?php
-	$conn = mysqli_connect("localhost","test","test12345","marcel");
-	if(!$conn)
-	{
-		echo "Database connection faild.";
-	}
+$servername = "localhost";
+$username = "test";
+$password = "test12345";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=marcel", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
 ?>
